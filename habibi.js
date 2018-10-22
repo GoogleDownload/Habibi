0db4d9884130b39d91e1849178f4bb035deffa68
Joseph Fanous <joseph.fanous@uoit.net>
2018-10-22 10:49:15 -0400

    Add files via upload

---
+++ habibi.js
@@ -0,0 +1,21 @@
+const Discord = require("discord.js");
+const client = new Discord.Client();
+const config = require("./config.json");
+
+client.on("ready", () => {
+  console.log("I am ready!");
+});
+client.on("message", (message) => {
+  if (!message.content.startsWith(config.prefix) || message.author.bot) return;
+
+  if (message.content.startsWith(config.prefix + "ping")) {
+    message.channel.send("pong!");
+  } else
+  if (message.content.startsWith(config.prefix + "foo")) {
+    message.channel.send("bar!");
+  } else
+  if (message.content.startsWith(config.prefix + "kys")) {
+    message.channel.send("ok 😢");
+  }
+});
+client.login(config.token);
